6b403145c2787cd4b125100ecafa0da871fc5389
wssccc <wssccc@qq.com>
2015-08-31 10:19:12 +0800

    Site updated: 2015-08-31 10:19:12

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
   $('a.blog-button').click(function() {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
-    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
-    $('.content-wrapper').addClass('animated slideInLeft');
+    // $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
+    // $('.content-wrapper').addClass('animated slideInLeft');
+    $('.panel-cover').addClass('panel-cover--collapsed');
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
